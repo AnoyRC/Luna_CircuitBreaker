5307c64f48b4bf75256a60a83255565697590b02
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2024-02-06 03:16:22 +0530

    Contracts : Luna Wallet Contract Added

---
+++ contracts/packages/hardhat/contracts/Luna.sol
@@ -0,0 +1,158 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./callback/TokenCallbackHandler.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "./verifier/PasskeyVerifier.sol";
+import "./verifier/RecoveryVerifier.sol";
+
+import "./utils/Conversion.sol";
+
+contract Luna is TokenCallbackHandler, Initializable {
+
+    string public DOMAIN;
+
+    bytes public passkeyInputs;
+
+    bytes32 public recoveryHash;
+    string public email;
+
+    PasskeyUltraVerifier public immutable PASSKEY_VERIFIER;
+    RecoveryUltraVerifier public immutable RECOVERY_VERIFIER;
+
+    uint256 private nonce;
+
+    constructor(
+        string memory domain_,
+        address passkeyVerifier_,
+        address recoveryVerifier_
+    ) {
+        DOMAIN = domain_;
+        PASSKEY_VERIFIER = PasskeyUltraVerifier(passkeyVerifier_);
+        RECOVERY_VERIFIER = RecoveryUltraVerifier(recoveryVerifier_);
+    }
+
+    function getNonce() public view returns (uint256) {
+        return nonce;
+    }
+
+    function _useNonce() internal returns (uint256) {
+        unchecked {
+            return nonce++;
+        }
+    }
+
+    function initialize(bytes memory _passkeyInputs, bytes32 _recoveryHash, string memory _email) external initializer {
+        _initialize(_passkeyInputs, _recoveryHash, _email);
+    }
+
+    function _initialize(bytes memory _passkeyInputs, bytes32 _recoveryHash, string memory _email) internal virtual {
+        passkeyInputs = _passkeyInputs;
+        recoveryHash = _recoveryHash;
+        email = _email;
+    }
+
+    function getCredentialId() public view returns (string memory) {
+        (, string memory credentialId, , , , ) = Conversion.decodeEncodedInputs(passkeyInputs);
+        return credentialId;
+    }
+
+    function verifyPasskey(bytes calldata proof) public view returns (bool) {
+        (bytes32 pubkeyHash, , bytes memory authenticatorData, bytes1 authenticatorDataFlagMask, bytes memory clientData, uint clientChallengeDataOffset) = Conversion.decodeEncodedInputs(passkeyInputs);
+        bytes32 message = Conversion.computeMessage( authenticatorData, authenticatorDataFlagMask, clientData, Strings.toString(getNonce()) , clientChallengeDataOffset);
+
+
+        bytes32[] memory inputs = Conversion.convertInputs(message, pubkeyHash);
+        return PASSKEY_VERIFIER.verify(proof, inputs);
+    }
+
+    function usePasskey(bytes calldata proof) internal returns (bool) {
+        (bytes32 pubkeyHash, , bytes memory authenticatorData, bytes1 authenticatorDataFlagMask, bytes memory clientData, uint clientChallengeDataOffset) = Conversion.decodeEncodedInputs(passkeyInputs);
+        bytes32 message = Conversion.computeMessage( authenticatorData, authenticatorDataFlagMask, clientData, Strings.toString(_useNonce()) , clientChallengeDataOffset);
+
+
+        bytes32[] memory inputs = Conversion.convertInputs(message, pubkeyHash);
+        return PASSKEY_VERIFIER.verify(proof, inputs);
+    }
+
+    function verifyRecovery(bytes calldata proof) public view returns (bool) {
+        bytes32 message = Conversion.hashMessage(Strings.toString(getNonce()));
+
+        bytes32[] memory _inputs = Conversion.convertInputs(message, recoveryHash);
+
+        return RECOVERY_VERIFIER.verify(proof, _inputs);
+    }
+
+    function useRecovery(bytes calldata proof) internal returns (bool) {
+        bytes32 message = Conversion.hashMessage(Strings.toString(_useNonce()));
+
+        bytes32[] memory _inputs = Conversion.convertInputs(message, recoveryHash);
+
+        return RECOVERY_VERIFIER.verify(proof, _inputs);
+    }
+
+    function execute(bytes calldata proof, address dest, uint256 value, bytes calldata func) external payable returns (bool) {
+        require(usePasskey(proof), "Invalid passkey");
+        _execute(dest, value, func);
+        return true;
+    }
+
+    function executeBatch(bytes calldata proof, address[] calldata dest, uint256[] calldata value, bytes[] calldata func) external payable returns (bool) {
+        require(usePasskey(proof), "Invalid passkey");
+        _executeBatch(dest, value, func);
+        return true;
+    }
+
+    function executePasskeyRecovery(bytes calldata proof, bytes memory _passkeyInputs) external payable returns (bool) {
+        require(useRecovery(proof), "Invalid recovery");
+        _changePasskeyInputs(_passkeyInputs);
+        return true;
+    }
+
+    function executeChangeRecovery(bytes calldata proof, bytes32 _recoveryHash, string memory _email) external payable returns (bool) {
+        require(useRecovery(proof), "Invalid recovery");
+        _changeRecovery(_recoveryHash, _email);
+        return true;
+    }
+
+    function _changePasskeyInputs(bytes memory _passkeyInputs) internal  {
+		passkeyInputs = _passkeyInputs;
+	}
+
+    function _changeRecovery(bytes32 _recoveryHash, string memory _email) internal  {
+        recoveryHash = _recoveryHash;
+        email = _email;
+    }
+
+    function _execute(address dest, uint256 value, bytes calldata func) internal {
+        _call(dest, value, func);
+    }
+
+    function _executeBatch(address[] calldata dest, uint256[] calldata value, bytes[] calldata func) internal {
+        require(dest.length == func.length && (value.length == 0 || value.length == func.length), "wrong array lengths");
+        if (value.length == 0) {
+            for (uint256 i = 0; i < dest.length; i++) {
+                _call(dest[i], 0, func[i]);
+            }
+        } else {
+            for (uint256 i = 0; i < dest.length; i++) {
+                _call(dest[i], value[i], func[i]);
+            }
+        }
+    }
+
+	function _call(address target, uint256 value, bytes memory data) internal {
+        (bool success, bytes memory result) = target.call{value : value}(data);
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
---
+++ contracts/packages/hardhat/contracts/utils/Conversion.sol
@@ -0,0 +1,139 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+library Conversion {
+    error InvalidAuthenticatorData();
+    error InvalidClientData();
+
+    function convertInputs(bytes32 message, bytes32 _hash) internal pure returns (bytes32 [] memory){
+        bytes32[] memory byte32Inputs = new bytes32[](33);
+
+        for (uint256 i = 0; i < 32; i++) {
+            byte32Inputs[i] = convertToPaddedByte32(message[i]);
+        }
+
+        byte32Inputs[32] = _hash;
+
+        return byte32Inputs;
+    }
+
+    function convertToPaddedByte32(bytes32 value) internal pure returns (bytes32) {
+        bytes32 paddedValue;
+        paddedValue = bytes32(uint256(value) >> (31 * 8));
+        return paddedValue;
+    } 
+
+    function hashMessage(string memory message) internal pure returns (bytes32) {
+        string memory messagePrefix = "\x19Ethereum Signed Message:\n";
+
+        string memory lengthString = Strings.toString(bytes(message).length);
+
+        string memory concatenatedMessage = string(abi.encodePacked(messagePrefix, lengthString, message));
+
+        return keccak256(bytes(concatenatedMessage));
+    }
+
+     function decodeEncodedInputs(
+        bytes memory _inputs
+    )
+        internal
+        pure
+        returns (bytes32 ,string memory, bytes memory, bytes1, bytes memory, uint)
+    {
+        (
+            bytes32 pubkeyHash,
+            string memory credentialId,
+            bytes memory authenticatorData,
+            bytes1 authenticatorDataFlagMask,
+            bytes memory clientData,
+            uint clientChallengeDataOffset
+        ) = abi.decode(_inputs, (bytes32, string, bytes, bytes1, bytes, uint));
+
+        return (
+            pubkeyHash,
+            credentialId,
+            authenticatorData,
+            authenticatorDataFlagMask,
+            clientData,
+            clientChallengeDataOffset
+        );
+    }
+
+    function computeMessage(
+        bytes memory authenticatorData,
+        bytes1 authenticatorDataFlagMask,
+        bytes memory clientData,
+        string memory clientChallenge,
+        uint clientChallengeDataOffset
+    ) internal pure returns (bytes32) {
+        if (
+            (authenticatorData[32] & authenticatorDataFlagMask) !=
+            authenticatorDataFlagMask
+        ) {
+            revert InvalidAuthenticatorData();
+        }
+
+        bytes memory challengeExtracted = new bytes(
+            bytes(clientChallenge).length
+        );
+
+        copyBytes(
+            clientData,
+            clientChallengeDataOffset,
+            challengeExtracted.length,
+            challengeExtracted,
+            0
+        );
+
+        if (
+            keccak256(abi.encodePacked(bytes(clientChallenge))) !=
+            keccak256(abi.encodePacked(challengeExtracted))
+        ) {
+            revert InvalidClientData();
+        }
+
+        bytes memory verifyData = new bytes(authenticatorData.length + 32);
+
+        copyBytes(
+            authenticatorData,
+            0,
+            authenticatorData.length,
+            verifyData,
+            0
+        );
+
+        copyBytes(
+            abi.encodePacked(sha256(clientData)),
+            0,
+            32,
+            verifyData,
+            authenticatorData.length
+        );
+
+        return (sha256(verifyData));
+    }
+
+    function copyBytes(
+        bytes memory _from,
+        uint _fromOffset,
+        uint _length,
+        bytes memory _to,
+        uint _toOffset
+    ) internal pure returns (bytes memory _copiedBytes) {
+        uint minLength = _length + _toOffset;
+        require(_to.length >= minLength);
+        uint i = 32 + _fromOffset; 
+        uint j = 32 + _toOffset;
+        while (i < (32 + _fromOffset + _length)) {
+            assembly {
+                let tmp := mload(add(_from, i))
+                mstore(add(_to, j), tmp)
+            }
+            i += 32;
+            j += 32;
+        }
+        return _to;
+    }
+}
